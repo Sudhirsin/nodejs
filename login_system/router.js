0be5c9ba25e82c7bc208ce0868c30045d63268f0
Sudhirsin <lion.sud.k@gmail.com>
2021-01-13 21:21:17 +0530

    logout feature added

--- login_system/router.js
+++ login_system/router.js
@@ -13,11 +13,32 @@ const credential = {
 router.post('/login', (req, res) => {
     if (req.body.email == credential.email && req.body.password == credential.password) {
         req.session.user = req.body.email
-        // res.redirect('/dashboard')
-        res.end('Login Success')
+        res.redirect('/route/dashboard')
+        // res.end('Login Success')
     } else {
         res.send('Invalid User')
     }
+})
+
+// route for dashboad
+router.get('/dashboard', (req, res) => {
+    if (req.session.user) {
+        res.render('dashboard', { user: req.session.user })
+    } else {
+        res.send('Invalid User name')
+    }
+});
+
+// route for logout
+router.get('/logout', (req, res) => {
+    req.session.destroy(function(err) {
+        if (err) {
+            console.log(err)
+            res.send('Error')
+        } else {
+            res.render('base', { title: "Express", logout: 'Logout Successfully..'})
+        }
+    })
 })
 
 module.exports = router;
